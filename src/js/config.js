0a57acc23d17b16074c327c3d95e6ba1943a080c
DRV111 <xblackcrownx@gmail.com>
2021-09-13 19:58:58 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '629c15cb-33e2-4a01-a049-568b0b58ec5e';
+export const MODAL_CLOSE_SEC = 2.5;
